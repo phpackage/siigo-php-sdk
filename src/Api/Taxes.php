2e0b10ac861c39e408ca31c16b9d7bea2e64d5d6
Manuel Mendoza <alejobit@gmail.com>
2020-04-16 14:40:05 -0500

    Add taxes api class

---
+++ src/Api/Taxes.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Phpackage\Siigo\Api;
+
+use Phpackage\Siigo\Client;
+use Phpackage\Siigo\Exception\InternalException;
+
+final class Taxes extends AbstractApi
+{
+    /**
+     * @param int $pageNumber
+     * @return array|null
+     * @throws InternalException
+     */
+    public function getAll(int $pageNumber = 0)
+    {
+        $query = http_build_query([
+            'namespace' => Client::NAMESPACE,
+            'numberPage' => $pageNumber,
+        ]);
+
+        return $this
+            ->getClient()
+            ->get(sprintf('%s/GetAll?%s', $this->getModelName(), $query));
+    }
+
+    /**
+     * @param string $code
+     * @return array|null
+     * @throws InternalException
+     */
+    public function getByCode(string $code)
+    {
+        $query = http_build_query([
+            'namespace' => Client::NAMESPACE,
+            'code' => $code,
+        ]);
+
+        return $this
+            ->getClient()
+            ->get(sprintf('%s/GetByCode?%s', $this->getModelName(), $query));
+    }
+
+    /**
+     * @param int $id
+     * @return array|null
+     * @throws InternalException
+     */
+    public function getById(int $id)
+    {
+        $query = http_build_query([
+            'namespace' => Client::NAMESPACE,
+        ]);
+
+        return $this
+            ->getClient()
+            ->get(sprintf('%s/GetByID/%s?%s', $this->getModelName(), $id, $query));
+    }
+}
